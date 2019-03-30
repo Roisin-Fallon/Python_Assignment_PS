7124d31ec95239e8b2ca3c279f862346642bc3a7
Roisin-Fallon <48020027+Roisin-Fallon@users.noreply.github.com>
2019-03-30 23:51:08 +0000

    Update question2.py

---
+++ Programming and Scripting/question2.py
@@ -0,0 +1,21 @@
+''' 
+Roisin Fallon, 28 Feb 2019
+
+Write a program that outputs whether or not today is a day that begins with the letter T. 
+
+An example of running this program on a Thursday is as follows. 
+$ python begins-with-t.py
+ Yes - today begins with a T.
+
+An example of running it on a Wednesday is as follows. 
+$ python begins-with-t.py 
+No - today does not begin with a T.
+
+Adapted from https://github.com/ianmcloughlin/python-tuesday/blob/master/tuesday.py
+'''
+
+import datetime #Import the datetime module 
+if datetime.datetime.now().weekday() in [1, 3]:    # Checks to see if the day is Tuesday or Thursday.
+  print("Yes - today begins with a T.")
+else:
+  print("No - today does not begin with a T.") 	   # The remainder of days in the week do not begin with T. 
