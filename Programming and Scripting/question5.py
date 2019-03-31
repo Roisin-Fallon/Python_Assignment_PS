b0932b940977fd63134d20cc19aee665d7e98851
Roisin-Fallon <48020027+Roisin-Fallon@users.noreply.github.com>
2019-03-31 21:01:46 +0100

    Complete

--- Programming and Scripting/question5.py
+++ Programming and Scripting/question5.py
@@ -14,22 +14,22 @@ while True:                                                                     
         n =int(input("Please enter a positive integer: "))                                     # Asks the user to input a positive integer. 
     
     except ValueError:                                                  
-        print("You have not entered a valid integer. Try Again!.")                             # The user has not entered a valid number (e.g. letter). Thus, it prints the following statement.
+        print("You have not entered a valid integer. Try Again!")                              # The user has not entered a valid number (e.g. letter). Thus, it prints the following statement.
         continue                                                                               # The loop starts again, prompts user to enter a positive integer. 
 
     if n < 1:                                                                                  # If the user inputs a negative number or zero. 
-        print("Prime numbers are integers greater than 1. Try Again!.")                        # Prompts the user that they have must give a number > 1
+        print("Prime numbers are integers greater than 1. Try Again!")                         # Prompts the user that they have must give a number > 1
         continue                                                                               # The loop starts again, prompts user to enter a positive integer.
     else:                                                                                      # User has given a valid number 
         break                                                                                  # Break- exit the loop
 
-if n == 1:                                                                                     # As n is by definition not a prime number. If statement added to state this. 
+if n == 1:                                                                                     # As n is by definition not a prime number. If statement added to state this 
         print(n, 'is not a prime number')  
 else:
         for x in range(2, n):                                                                  # Loop through the numbers 2 (including 2) up to and including n (user input).
-                if n % x == 0:                                                                 # Determine if n has a factor  i.e. is the number divisible by any number other than 1 and itself.                     
+                if n % x == 0:                                                                 # Determine if n has a factor  i.e. is the number divisible by any number other than 1 and itself. With each loop x is incremented by 1                   
                         print("This is not a prime number as", n,  "has a factor of", n//x)    # Displayed to user
-                        break                                                                  # Breaks -exits the inner for loop as it does not need to determine all the factors as this not what the algorithm is looking for.
+                        break                                                                  # Breaks -exits the inner for loop as it does not need to determine all the factors as this not what the algorithm is looking for
                                                                   
         else:
                 print(n, 'is a prime number')                                                  # Loop fell through without finding a factor which makes it a prime number!!
