9495838f1dcaf1bf3d649a137ea836424efc60d4
Roisin-Fallon <48020027+Roisin-Fallon@users.noreply.github.com>
2019-03-31 03:31:37 +0100

    Add comments

--- Programming and Scripting/question6.py
+++ Programming and Scripting/question6.py
@@ -6,18 +6,24 @@ $ python secondstring.py
 Please enter a sentence: The quick brown fox jumps over the lazy dog. 
 The brown jumps the dog
 
-Adapted from http://www.pitt.edu/~naraehan/python3/split_join.html
+Adapted from https://stackoverflow.com/a/509295; http://www.pitt.edu/~naraehan/python3/split_join.html
 Adapted to remove full stops and commas https://stackoverflow.com/a/3559576
 '''
 
-x = (input('Please enter a sentence: '))            # Request to user to input a sentence
-x = x.replace(',', '')                              # removes commas in the sentene entered by the user
-x = x.replace('.', '')                              # removes full stops in the sentene entered by the user
+x = (input('Please enter a sentence: '))                   # Request to user to input a sentence
+x = x.replace(',', '')                                     # Removes commas in the sentene entered by the user
+x = x.replace('.', '')                                     # Removes full stops in the sentene entered by the user
 
-everytwo = ' '.join(x.split()[::2])                 # .split() method returns a list of strings after breaking the given string by the specified separator.
-                                                 # Join is used to put the list of words back together in a single string with a space in between (' ')
-                                                # [start position: length of string: sequence]. in this instance i am using the default for the first 2 and 2 for the last one as I am looking for every second word. 
+everytwo = ' '.join(x.split()[::2])                        # .split() method returns a list of strings after breaking the given string by the specified separator.
+                                                           # [start: stop: step] * This is explained further below
+                                                           # Join is used to put the list of words back together in a single string with a space in between (' ')
+                                                            
+
 print(everytwo)                                             # Prints every second word 
 
 # It was necessary to remove the full stop from the sentence in the example given in the question. 
 # I also removed commas from the sentence as it is another common punctuation included in sentences. 
+
+# [start: stop: step] start is what the position to start. In this instance I have entered no value so it will default to position zero.
+# stop refers to what position on the string you want to finish. Again because I want the full string to be included I will use the default value.
+# step refers to increments i.e. if you want every second word it would be 2. 
