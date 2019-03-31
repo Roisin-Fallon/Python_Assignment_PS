b6e463f90255e73a1a707831d1a7c62b1175ebbb
Roisin-Fallon <48020027+Roisin-Fallon@users.noreply.github.com>
2019-03-31 21:16:49 +0100

    Complete

--- Programming and Scripting/question8.py
+++ Programming and Scripting/question8.py
@@ -1,31 +1,22 @@
+'''
+Roisin Fallon 20 March 2019
 
-# Reference https://docs.python.org/2/library/datetime.html
+Write a program that outputs today’s date and time in the format “Monday,January 10th 2019 at 1:15pm”. 
+Adapted from https://docs.python.org/3.4/library/datetime.html?highlight=weekday
+'''
 
-import datetime as dt
+import datetime as dt                                                                               # import datetime module with an alias dt 
+                                                                    
+now = dt.datetime.now()                                                                             # To get the current date and time when the program is run 
+date_string = now.strftime('%A, %B # %Y at %#I:%M%p')                                               # Detailed explanation of this is included in README 
+month_date = now.day
 
-# Current date and time.
-print(dt.datetime.strftime(dt.datetime.now(), "%A, %B %d %Y at %I:%M %p"))
+if (3 < month_date< 21) or (23 < month_date < 31):                                                  # Created an if statement to deal with the suffix related to the month date.
+  month_date = str(month_date) + 'th'                                                               # th is associated with with the days 
+else:
+  suffixes = {1: 'st', 2: 'nd', 3: 'rd'}
+  month_date = str(month_date) + suffixes[month_date % 10]                                          # Divide the day by 10, if you get a remainder of: 1 (st suffix); 2 (nd suffix); 3 (rd suffix)  
+
+print(date_string.replace('#', month_date).replace('PM','pm').replace('AM','am'), end='')           # Print the current date and time in the desired format. 
 
 
-def S(self):
-    
-    if self.data.day in (11, 12, 13):  # Special case
-        return 'th'
-    last = self.data.day % 10
-    if last == 1:
-        return 'st'
-    if last == 2:
-        return 'nd'
-    if last == 3:
-        return 'rd'
-    return 'th'
-
-#def date_suffix():
-   # if %d in [1, 21, 31]: 
-    #    suffix = 'st'
-    #elif %d in [2, 22]:
-     #   suffix = 'nd'
-    #elif %d in [3, 23, 33]:
-     #   suffix = 'rd'
-    #else:
-     #   suffix = 'th'
