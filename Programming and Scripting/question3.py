ad1cfb675b932654e8c7b1d62fa0dcbf6b780c92
Roisin-Fallon <48020027+Roisin-Fallon@users.noreply.github.com>
2019-03-31 20:52:05 +0100

    Complete

--- Programming and Scripting/question3.py
+++ Programming and Scripting/question3.py
@@ -8,15 +8,15 @@ $ python divisors.py
 1026 etc 
 9990
 
-Format adopted from: https://stackoverflow.com/a/45367591 (used to include commas when numbers are in there thousands)
+Adapted from: https://pynative.com/python-range-function/; https://stackoverflow.com/a/45367591
 '''
 
-for x in range(1000, 10001): 		# x increases by 1 each time it loops, it starts at 1000 and ends 10001 (explanation of why below)
-    if (x % 6 == 0 and x % 12 != 0): 	# To determine if they are divisible by 6 and not divisible by 12
-       print(f"{x:,d}") 			# Format numbers with commas. This line can be omitted from the code without affecting the code.
+for x in range(1000, 10001): 		     # x increases by 1 each time it loops, it starts at 1000 and ends 10001 (explanation of why below)
+    if (x % 6 == 0 and x % 12 != 0): 	 # To determine if they are divisible by 6 and not divisible by 12
+       print(f"{x:,d}") 			     # Format numbers with commas. This line can be omitted from the code without affecting the code.
   
         
 # In this question they ask for the numbers between 1000 and 10000, in using the Python compiler (link in README) I noticed the 
 # last number than ran through the for loop was 9999 which is 1 less than the question requested. On further research I realised that the range(n) 
 # is of exclusive nature and hence will not include last number (10000) in the output. 
-# To rectify this I increased 10000 by 1 which gave me a new range of (1000, 10001). Adapted from https://pynative.com/python-range-function/ ''''
+# To rectify this I increased 10000 by 1 which gave me a new range of (1000, 10001). 
