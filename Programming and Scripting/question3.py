2e7503428b037d3f821757a55b0981ac68f29261
Roisin-Fallon <48020027+Roisin-Fallon@users.noreply.github.com>
2019-03-31 00:19:55 +0000

    Add comments

--- Programming and Scripting/question3.py
+++ Programming and Scripting/question3.py
@@ -1,13 +1,22 @@
+'''
+Roisin Fallon 20 Feb 2019 
 
+Write a program that prints all numbers between 1,000 and 10,000 that are divisible by 6 but not 12. 
+$ python divisors.py 
+1002 
+1014 
+1026 etc 
+9990
 
-# To get numbers between 1000 and 10000 we use the range function 
+Format adopted from: https://stackoverflow.com/a/45367591 (used to include commas when numbers are in there thousands)
+'''
 
-for x in range(100, 10000):
-
-
-# To determine if they are divisible by 6 and not divisible by 12
-    if (x % 6 == 0 and x % 12 != 0):
-
-# To format the numbers printed with commas where necessary. Adopted from 43: https://stackoverflow.com/questions/1823058/how-to-print-number-with-commas-as-thousands-separators 
-       print(f"{x:,d}")
-   
+for x in range(1000, 10001): 		# x increases by 1 each time it loops, it starts at 1000 and ends 10001 (explanation of why below)
+    if (x % 6 == 0 and x % 12 != 0): 	# To determine if they are divisible by 6 and not divisible by 12
+       print(f"{x:,d}") 			# Format numbers with commas. This line can be omitted from the code without affecting the code.
+  
+        
+# In this question they ask for the numbers between 1000 and 10000, in using the Python compiler (link in README) I noticed the 
+# last number than ran through the for loop was 9999 which is 1 less than the question requested. On further research I realised that the range(n) 
+# is of exclusive nature and hence will not include last number (10000) in the output. 
+# To rectify this I increased 10000 by 1 which gave me a new range of (1000, 10001). Adapted from https://pynative.com/python-range-function/ ''''
